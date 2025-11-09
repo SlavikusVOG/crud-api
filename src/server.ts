00c2c59c6375239b90311966b5dfb2440f59be4a
SlavikusVOG <slavaolish@gmail.com>
2025-11-10 01:00:27 +0300

    refactor: improve code

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,8 @@ const server = createServer(async(req: IncomingMessage, res: ServerResponse) => 
   if (pathname.startsWith('/api/users')) {
     usersRoute.handleRequest(req, res);
   }
+  res.writeHead(404);
   res.end(constants.NOT_FOUND_MESSAGE);
-  res.end('Not found');
 });
 
 server.listen(PORT, () => {
