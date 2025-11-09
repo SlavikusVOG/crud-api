7f26d6be6db5033246dc1d442964ed5bfe7467ec
SlavikusVOG <slavaolish@gmail.com>
2025-11-09 21:39:16 +0300

    feat: add user route

---
+++ src/routes/users.ts
@@ -0,0 +1,21 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+
+import { db } from '../db/db.js';
+import type { User } from '../types/user.types.js';
+import { parse } from "node:url";
+import utils from "../utils/utils.js";
+
+function handleRequest(req: IncomingMessage, res: ServerResponse) {
+  const method = req.method;
+  const url = utils.getURL(req);
+  const pathname = url.pathname;
+  switch(method) {
+    case 'GET': {
+      break;
+    }
+  }
+}
+
+export default {
+  handleRequest
+}
