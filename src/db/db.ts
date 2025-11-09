885e582979d509154d6dbb2b5c83eb9277c3478a
SlavikusVOG <slavaolish@gmail.com>
2025-11-09 17:27:00 +0300

    feat: implement db with crud operations

---
+++ src/db/db.ts
@@ -0,0 +1,32 @@
+import type { User } from "../types/user.types.js";
+
+export class DB {
+  private users: Map<string, User> = new Map();
+
+  create(user: User): User {
+    this.users.set(user.id, user);
+    return user;
+  }
+
+  getUsers(): User[] {
+    return Array.from(this.users.values());
+  }
+
+  getUserById(id: string): User|undefined {
+    return this.users.get(id);
+  }
+
+  updateUser(id: string, data: User): User|undefined {
+    const user = this.users.get(id);
+    if (!user) return undefined;
+    const updatedUser = { ...user, ...data};
+    this.users.set(id, updatedUser);
+    return updatedUser;
+  }
+
+  delete(id: string) {
+    return this.users.delete(id);
+  }
+}
+
+export const db = new DB();
