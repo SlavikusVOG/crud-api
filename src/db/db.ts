5a2cdfe9d32d30f19c453b2c3719157f2bc4f43e
SlavikusVOG <slavaolish@gmail.com>
2025-11-10 01:50:59 +0300

    feat: improve database

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,36 @@
+import { join } from "node:path";
 import type { User } from "../types/user.types.js";
+import { v4 as uuidv4 } from 'uuid';
+import { readFile, writeFile } from "node:fs/promises";
+
+const directory = import.meta.dirname;
+const dbDirname= join( directory, 'database');
+
+const DB_PATH = join(dbDirname, 'users.json');
 
 export class DB {
   private users: Map<string, User> = new Map();
 
-  create(user: User): User {
-    this.users.set(user.id, user);
+  // TODO: move DB to file
+  async load(): Promise<User[]> {
+    try {
+      const data = await readFile(DB_PATH);
+      return JSON.parse(data.toString())
+    }
+    catch {
+      return [];
+    }
+  }
+
+  // TODO: move DB to file
+  async save(users: User[]): Promise<void> {
+    await writeFile(DB_PATH, JSON.stringify(users));
+  }
+
+  create(data: Omit<User, 'id'>): User {
+    const id: string = uuidv4();
+    const user: User = {...data, ...{ id }}
+    this.users.set(id, user);
     return user;
   }
 
