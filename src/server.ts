0775334db95767b60f8712b429344e213e2420c7
SlavikusVOG <slavaolish@gmail.com>
2025-11-09 21:38:48 +0300

    feat: implement server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import dotenv from 'dotenv';
+import usersRoute from './routes/users.js';
+import utils from './utils/utils.js';
+
+dotenv.config();
+const PORT = Number(process.env.PORT) || 4000;
+
+const server = createServer(async(req: IncomingMessage, res: ServerResponse) => {
+  const url = utils.getURL(req);
+  const pathname = url?.pathname;
+  if (pathname.startsWith('/api/users')) {
+    usersRoute.handleRequest(req, res);
+  }
+  res.statusCode = 404;
+  res.end('Not found');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+})
