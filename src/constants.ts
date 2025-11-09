1c1071b784d029603b58b05949383244810f4cca
SlavikusVOG <slavaolish@gmail.com>
2025-11-10 01:00:44 +0300

    feat: add constants messages

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const constants = {
   NOT_FOUND_MESSAGE: 'Not found',
+  SERVER_ERROR_MESSAGE: 'Error on server side',
+  USER_NOT_FOUND_MESSAGE: 'User not found',
+  INVALID_ID_MESSAGE: 'Invalid user id',
+  NO_REQUIRED_FIELDS_MESSAGE: 'Data do not contained required fields',
 };
